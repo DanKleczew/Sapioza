01b70a7323738efd94bed50ea5b7cfb6667734fb
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2025-01-20 15:54:07 +0100

    prettier

--- paper-service/src/main/java/fr/pantheonsorbonne/dto/PaperDTOs/AlteredPaperDTO.java
+++ paper-service/src/main/java/fr/pantheonsorbonne/dto/PaperDTOs/AlteredPaperDTO.java
@@ -1,4 +1,8 @@
 package fr.pantheonsorbonne.dto.PaperDTOs;
 
-public record AlteredPaperDTO(Long paperId, String body, Long authorId) {
+public record AlteredPaperDTO(
+        Long paperId,
+        String body,
+        Long authorId) {
+    // Updated paper body for pdf generation
 }
